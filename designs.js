ac408eb72a1467ed076cb053ce718cc87512368a
jempitman <80392341+jempitman@users.noreply.github.com>
2021-03-18 12:27:37 +0100

    Update to build table dynamically

--- designs.js
+++ designs.js
@@ -1,6 +1,9 @@
 // Select color input
+
+
 // Select size input
 function selectSizeInput(){
+
   // fetch inputHeight from DOM
   let height = parseInt(document.getElementById("inputHeight").value);
   //console.log("Required height is " + height);
@@ -9,38 +12,26 @@ function selectSizeInput(){
   let width = parseInt(document.getElementById("inputWidth").value);
   //console.log("Required width is " + width);
 
-  return [height, width];
+  makeGrid(height, width);
 
 }
+
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-  // call selectSizeInput function to fetch Height
-  // and Width measurements from input dialogs
-  let size = selectSizeInput();
-
-  //extract height and width measurements
-  let height = size[0];
-  let width = size[1];
+function makeGrid(height, width) {
 
   //Capture pixelCanvas from DOM
   let table = document.getElementById("pixelCanvas");
 
+  //Initializing table for dynamic build
+  table.innerHTML = "";
+
   // outer for loop to create rows
   for (let i = 0; i < height; i++){
-
-    // checking rows are being created correctly
-    //console.log("Creating row " + i);
 
     //create table row object
     let row = document.createElement("tr");
 
     for (let j = 0; j < width; j++){
-
-      //checking columns are being created correctly
-      //console.log("Creating column " + j)
-
       //create data object cells (columns of <td>)
       //and append to end of table row
       let cell = document.createElement("td");
